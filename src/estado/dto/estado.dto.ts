86bc098c6091732c4ce9ab545e11c4fe78bf96ea
unknown <collegek450@gmail.com>
2024-12-07 08:55:28 -0500

    feat: udistrital/sisifo_documentacion#255 Nuevo atributo en la tabla plan estado: rol_usuario

--- src/estado/dto/estado.dto.ts
+++ src/estado/dto/estado.dto.ts
@@ -1,25 +1,27 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class PlanEstadoDto {
-    @ApiProperty()
-    readonly plan_auditoria_id: string;
+  @ApiProperty()
+  readonly plan_auditoria_id: string;
 
-    @ApiProperty()
-    readonly usuario_id: number;
+  @ApiProperty()
+  readonly usuario_id: number;
 
-    @ApiProperty()
-    readonly observacion: string;
+  @ApiProperty()
+  readonly usuario_rol: string;
 
-    @ApiProperty()
-    readonly actual: boolean;
+  @ApiProperty()
+  readonly observacion: string;
 
-    @ApiProperty()
-    readonly estado_id: number;
+  @ApiProperty()
+  readonly actual: boolean;
 
-    @ApiProperty()
-    readonly fecha_ejecucion_estado: Date;
+  @ApiProperty()
+  readonly estado_id: number;
 
-    @ApiProperty()
-    activo: boolean;
+  @ApiProperty()
+  readonly fecha_ejecucion_estado: Date;
 
+  @ApiProperty()
+  activo: boolean;
 }
--- src/estado/schema/estado.schema.ts
+++ src/estado/schema/estado.schema.ts
@@ -1,30 +1,32 @@
 import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
-import { PlanAuditoria } from '../../plan-auditoria/schemas/plan-auditoria.schema'
+import { PlanAuditoria } from '../../plan-auditoria/schemas/plan-auditoria.schema';
 //cuantos estados pueden estar activos por plan de auditorita al tiempo?
 @Schema({ collection: 'plan-estado' })
 export class PlanEstado extends Document {
-    @Prop({ required: false, type: Types.ObjectId, ref: PlanAuditoria.name})
-    plan_auditoria_id: PlanAuditoria | Types.ObjectId;
+  @Prop({ required: false, type: Types.ObjectId, ref: PlanAuditoria.name })
+  plan_auditoria_id: PlanAuditoria | Types.ObjectId;
 
-    @Prop({ required: false})
-    usuario_id: number;
+  @Prop({ required: false })
+  usuario_id: number;
 
-    @Prop({ required: false })
-    observacion: string;
+  @Prop({ required: false })
+  usuario_rol: string;
 
-    @Prop({ required: false })
-    actual: boolean;
+  @Prop({ required: false })
+  observacion: string;
 
-    @Prop({ required: false })
-    estado_id: number;
+  @Prop({ required: false })
+  actual: boolean;
 
-    @Prop({ required: false })
-    fecha_ejecucion_estado: Date;
+  @Prop({ required: false })
+  estado_id: number;
 
-    @Prop({ required: false })
-    activo: boolean;
+  @Prop({ required: false })
+  fecha_ejecucion_estado: Date;
 
+  @Prop({ required: false })
+  activo: boolean;
 }
 
 export const PlanEstadoSchema = SchemaFactory.createForClass(PlanEstado);
