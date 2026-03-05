b98d4cdeda352c9b32b34fbb0926ed2731141861
MrPocketMonsters <ndsabogalv@gmail.com>
2026-03-04 19:31:43 -0500

    feat: Implementación inicial aislada de AuditoriaPadreModule udistrital/sisifo_documentacion#557

---
+++ src/auditoria-padre/auditoria-padre.controller.ts
@@ -0,0 +1,191 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Query,
+  Res,
+} from '@nestjs/common';
+import { AuditoriaPadreService } from './auditoria-padre.service';
+import { AuditoriaPadreDTO } from './dto/auditoria-padre.dto';
+import { FilterDto } from '../filters/filters.dto';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiParam,
+  ApiBody,
+} from '@nestjs/swagger';
+import { ParseObjectIdPipe } from '../pipes/parse-object-id/parse-object-id.pipe';
+
+@ApiTags('auditoria-padre')
+@Controller('auditoria-padre')
+export class AuditoriaPadreController {
+  constructor(private AuditoriaPadreService: AuditoriaPadreService) {}
+
+  @Post()
+  @ApiOperation({ summary: 'Crear una nueva auditoria padre' })
+  @ApiBody({ type: AuditoriaPadreDTO })
+  @ApiResponse({
+    status: 201,
+    description: 'La auditoria padre ha sido creada exitosamente.',
+    type: AuditoriaPadreDTO,
+  })
+  @ApiResponse({ status: 400, description: 'Solicitud incorrecta.' })
+  async post(
+    @Res() res,
+    @Body(new ParseObjectIdPipe(['plan_auditoria_id']))
+    auditoriaPadreDTO: AuditoriaPadreDTO,
+  ) {
+    try {
+      const auditoriaPadre =
+        await this.AuditoriaPadreService.post(auditoriaPadreDTO);
+      res.status(HttpStatus.CREATED).json({
+        Success: true,
+        Status: HttpStatus.CREATED,
+        Message: 'Registro Exitoso',
+        Data: auditoriaPadre,
+      });
+    } catch (error) {
+      res.status(HttpStatus.BAD_REQUEST).json({
+        Success: false,
+        Status: HttpStatus.BAD_REQUEST,
+        Message:
+          'Error servicio Post: la solicitud contiene un tipo de dato incorrecto o un parametro invalido',
+        Data: error.message,
+      });
+    }
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Obtener todas las auditorias padre' })
+  @ApiResponse({
+    status: 200,
+    description: 'Devuelve todas las auditorias padre.',
+    type: [AuditoriaPadreDTO],
+  })
+  async getAll(@Res() res, @Query() filterDto: FilterDto) {
+    try {
+      const auditoriasPadre =
+        await this.AuditoriaPadreService.getAll(filterDto);
+      const counts = await this.AuditoriaPadreService.count(filterDto);
+
+      res.status(HttpStatus.OK).json({
+        Success: true,
+        Status: HttpStatus.OK,
+        Message: 'Peticion Exitosa',
+        Data: auditoriasPadre,
+        MetaData: { Count: counts },
+      });
+    } catch (error) {
+      res.status(HttpStatus.NOT_FOUND).json({
+        Success: false,
+        Status: HttpStatus.NOT_FOUND,
+        Message:
+          'Error en servicio GetAll: la peticion contiene un parametro incorrecto o no existe un registro',
+        Data: error.message,
+      });
+    }
+  }
+
+  @Get('/:id')
+  @ApiOperation({ summary: 'Obtener una auditoria padre por Id' })
+  @ApiParam({ name: 'id', type: 'string' })
+  @ApiResponse({
+    status: 200,
+    description: 'Devuelve la auditoria padre.',
+    type: AuditoriaPadreDTO,
+  })
+  @ApiResponse({ status: 404, description: 'Auditoria padre no encontrada.' })
+  async getById(@Res() res, @Param('id') id: string) {
+    try {
+      const auditoriaPadre = await this.AuditoriaPadreService.getById(id);
+      res.status(HttpStatus.OK).json({
+        Success: true,
+        Status: HttpStatus.OK,
+        Message: 'Peticion Exitosa',
+        Data: auditoriaPadre,
+      });
+    } catch (error) {
+      res.status(HttpStatus.NOT_FOUND).json({
+        Success: false,
+        Status: HttpStatus.NOT_FOUND,
+        Message:
+          'Error en servicio GetOne: la peticion contiene un parametro incorrecto o no existe un registro',
+        Data: error.message,
+      });
+    }
+  }
+
+  @Put('/:id')
+  @ApiOperation({ summary: 'Actualizar una auditoria padre' })
+  @ApiParam({ name: 'id', type: 'string' })
+  @ApiBody({ type: AuditoriaPadreDTO })
+  @ApiResponse({
+    status: 200,
+    description: 'La auditoria padre ha sido actualizada exitosamente.',
+    type: AuditoriaPadreDTO,
+  })
+  @ApiResponse({ status: 400, description: 'Solicitud incorrecta.' })
+  @ApiResponse({ status: 404, description: 'Auditoria padre no encontrada.' })
+  async put(
+    @Res() res,
+    @Param('id') id: string,
+    @Body() auditoriaPadreDTO: AuditoriaPadreDTO,
+  ) {
+    try {
+      const auditoriaPadre = await this.AuditoriaPadreService.put(
+        id,
+        auditoriaPadreDTO,
+      );
+      res.status(HttpStatus.OK).json({
+        Success: true,
+        Status: HttpStatus.OK,
+        Message: 'Actualizacion Exitosa',
+        Data: auditoriaPadre,
+      });
+    } catch (error) {
+      res.status(HttpStatus.BAD_REQUEST).json({
+        Success: false,
+        Status: HttpStatus.BAD_REQUEST,
+        Message:
+          'Error en servicio Put: la peticion contiene un tipo de dato incorrecto o un parametro invalido',
+        Data: error.message,
+      });
+    }
+  }
+
+  @Delete('/:id')
+  @ApiOperation({ summary: 'Eliminar una auditoria padre' })
+  @ApiParam({ name: 'id', type: 'string' })
+  @ApiResponse({
+    status: 200,
+    description: 'La auditoria padre ha sido eliminada exitosamente.',
+  })
+  @ApiResponse({ status: 404, description: 'Auditoria padre no encontrada.' })
+  async delete(@Res() res, @Param('id') id: string) {
+    try {
+      await this.AuditoriaPadreService.delete(id);
+      res.status(HttpStatus.OK).json({
+        Success: true,
+        Status: HttpStatus.OK,
+        Message: 'Eliminacion Exitosa',
+        Data: {
+          _id: id,
+        },
+      });
+    } catch (error) {
+      res.status(HttpStatus.NOT_FOUND).json({
+        Success: false,
+        Status: HttpStatus.NOT_FOUND,
+        Message:
+          'Error en el servicio Delete: la peticion contiene parametros incorrectos',
+        Data: error.message,
+      });
+    }
+  }
+}
---
+++ src/auditoria-padre/auditoria-padre.service.ts
@@ -0,0 +1,111 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { FilterDto } from '../filters/filters.dto';
+import { FiltersService } from '../filters/filters.service';
+import { AuditoriaPadre } from './schemas/auditoria-padre.schema';
+import { AuditoriaPadreDTO } from './dto/auditoria-padre.dto';
+import { PlanAuditoria } from '../plan-auditoria/schemas/plan-auditoria.schema';
+
+@Injectable()
+export class AuditoriaPadreService {
+  constructor(
+    @InjectModel(AuditoriaPadre.name)
+    private readonly AuditoriaPadreModel: Model<AuditoriaPadre>,
+    @InjectModel(PlanAuditoria.name)
+    private readonly PlanAuditoriaModel: Model<PlanAuditoria>,
+  ) {}
+
+  private populateFields(): any[] {
+    return [{ path: 'plan_auditoria_id' }];
+  }
+
+  private async checkRelated(auditoriaPadreDTO: AuditoriaPadreDTO) {
+    if (auditoriaPadreDTO.plan_auditoria_id) {
+      const planAuditoria = await this.PlanAuditoriaModel.findById(
+        auditoriaPadreDTO.plan_auditoria_id,
+      ).exec();
+      if (!planAuditoria) {
+        throw new Error(
+          `Plan auditoria relacionada con id ${auditoriaPadreDTO.plan_auditoria_id} no existe`,
+        );
+      }
+    }
+  }
+
+  async post(auditoriaPadreDTO: AuditoriaPadreDTO): Promise<AuditoriaPadre> {
+    const fecha = new Date();
+    const auditoriaPadreData = {
+      ...auditoriaPadreDTO,
+      activo: true,
+      fecha_creacion: fecha,
+      fecha_modificacion: fecha,
+    };
+    await this.checkRelated(auditoriaPadreDTO);
+    return await this.AuditoriaPadreModel.create(auditoriaPadreData);
+  }
+
+  async getAll(filterDto: FilterDto): Promise<AuditoriaPadre[]> {
+    const filtersService = new FiltersService(filterDto);
+    let populateFields = [];
+    if (filtersService.isPopulated()) {
+      populateFields = this.populateFields();
+    }
+    return (await this.AuditoriaPadreModel.find(
+      filtersService.getQuery(),
+      filtersService.getFields() as any,
+      filtersService.getLimitAndOffset(),
+    )
+      .sort(filtersService.getSortBy())
+      .populate(populateFields)
+      .lean()
+      .exec()) as unknown as AuditoriaPadre[];
+  }
+
+  async getById(id: string): Promise<AuditoriaPadre> {
+    const auditoriaPadre = await this.AuditoriaPadreModel.findById(id).exec();
+    if (!auditoriaPadre) {
+      throw new Error(`${id} no existe`);
+    }
+    return auditoriaPadre;
+  }
+
+  async put(
+    id: string,
+    auditoriaPadreDTO: AuditoriaPadreDTO,
+  ): Promise<AuditoriaPadre> {
+    auditoriaPadreDTO.fecha_modificacion = new Date();
+    if (auditoriaPadreDTO.fecha_creacion) {
+      delete auditoriaPadreDTO.fecha_creacion;
+    }
+    await this.checkRelated(auditoriaPadreDTO);
+    const update = await this.AuditoriaPadreModel.findByIdAndUpdate(
+      id,
+      auditoriaPadreDTO,
+      { new: true },
+    ).exec();
+    if (!update) {
+      throw new Error(`${id} no existe`);
+    }
+    return update;
+  }
+
+  async delete(id: string): Promise<AuditoriaPadre> {
+    const deleted = await this.AuditoriaPadreModel.findByIdAndUpdate(
+      id,
+      { activo: false },
+      { new: true },
+    ).exec();
+    if (!deleted) {
+      throw new Error(`${id} no existe`);
+    }
+    return deleted;
+  }
+
+  async count(filterDto: FilterDto): Promise<number> {
+    const filtersService = new FiltersService(filterDto);
+    return await this.AuditoriaPadreModel.countDocuments(
+      filtersService.getQuery(),
+    ).exec();
+  }
+}
